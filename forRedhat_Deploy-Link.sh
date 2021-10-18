22e4c5c99d6f2cba617cb02bb7232bd04d4d7525
qayet1310 <dirkkuyt18mf@gmail.com>
2021-10-18 15:15:47 +0900

    add files for some distributions

---
+++ forRedhat_Deploy-Link.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "
+   ___           __             ___       __    _____ __
+  / _ |___ ___  / /__  __ __   / _ |___  / /_  / __(_) /__ ___
+ / // / -_) _ |/ / _ |/ // /  / // / _ |/ __/ / _// / / -_|_-<  _
+/____/l__/ .__/_/|___/|_, /  /____/|___/|__/ /_/ /_/_/|__/___/ (_)
+        /_/          /___/
+"
+
+Cpath=`pwd`
+
+echo Running Update.
+sudo apt-get update
+
+echo 
+echo Running upgrade.
+sudo apt-get upgrade
+
+echo 
+echo Installing packages.
+sudo apt install -y \
+build-essential \
+libffi-dev \
+libssl-dev \
+zlib1g-dev \
+liblzma-dev \
+libbz2-dev \
+libreadline-dev \
+libsqlite3-dev \
+libopencv-dev \
+tk-dev \
+git \
+vim
+
+echo Installing pyenv.
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+cd ~/.pynev
+git checkout v2.0.3
+
+# .bashrcの更新
+echo Updating .bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+source ~/.bashrc
+
+# virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+echo 
+echo Installing the colorscheme: vim-atom-dark
+mkdir ~/.vim/colors
+git clone https://github.com/gosukiwi/vim-atom-dark 
+cp ./vim-atom-dark/colors/atom-dark-256.vim ~/.vim/colors
+
+
+cd $Cpath
+
+
+for f in .??*
+do
+    ln -sf $Cpath/$f ~/$f
+    echo 'created link ['$f']'
+done
